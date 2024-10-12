483b0a755d8a544fca561bf7ff5e06dcb357c7e2
Quarnyx <anugrahsandysulmanpratama@gmail.com>
2024-10-12 17:52:11 +0700

    revisi

--- pages/kartu-persediaan/index.php
+++ pages/kartu-persediaan/index.php
@@ -5,7 +5,26 @@
     $maintitle = "Laporan";
     $title = 'Kartu Persediaan';
     include 'config.php';
-    $sql = "SELECT * FROM v_inventory ORDER BY tanggal_masuk ASC";
+    $sql = "SELECT
+    pb.tanggal_masuk AS date,
+    pb.kode_pembelian AS kode,
+    pb.jumlah AS jumlah_beli,
+    pb.harga_beli,
+		pj.jumlah AS jumlah_jual,
+    pj.harga_jual
+    FROM pembelian pb
+    LEFT JOIN penjualan pj ON pj.tanggal_penjualan = pb.tanggal_masuk AND pj.id_produk=pb.id_produk
+    UNION
+    SELECT
+    pj.tanggal_penjualan AS date,
+    pj.kode_penjualan AS kode,
+		pb.jumlah AS jumlah_beli,
+    pb.harga_beli,
+    pj.jumlah AS jumlah_jual,
+    pj.harga_jual
+    FROM penjualan pj
+    LEFT JOIN pembelian pb ON pb.tanggal_masuk = pj.tanggal_penjualan AND pb.id_produk=pj.id_produk
+    ORDER BY date ASC";
     $result = $conn->query($sql);
 
 
@@ -14,36 +33,58 @@
 
     ?>
     <div class="row">
-        <div class="mt-3 mb-3 d-print-none">
-            <a href="javascript:window.print()" class="btn btn-primary waves-effect waves-light"><i
-                    class="mdi mdi-printer me-1"></i> Print</a>
-        </div>
-        <?php
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Kartu Persediaan</h4>
+                    <div class="table-responsive">
+                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap"
+                            style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th rowspan="2">Tanggal</th>
+                                    <th rowspan="2">Kode</th>
+                                    <th colspan="3">Masuk</th>
+                                    <th colspan="3">Keluar</th>
+                                    <th colspan="3">Persediaan</th>
+                                </tr>
+                                <tr>
+                                    <th>Qty</th>
+                                    <th>Harga</th>
+                                    <th>Total Harga</th>
+                                    <th>Qty</th>
+                                    <th>Harga</th>
+                                    <th>Total Harga</th>
+                                    <th>Qty</th>
+                                    <th>Harga</th>
+                                    <th>Total Harga</th>
+                                </tr>
+                            </thead>
 
-        while ($row = $result->fetch_assoc()) {
-            ?>
-            <div class="col-md-4 col-lg-4 col-4">
-                <div class="card border border-success">
-                    <div class="card-header bg-transparent border-success">
-                        <h5 class="my-0 text-success"><i class="mdi mdi-check-all me-3"></i>Kode
-                            <?= $row['kode_pembelian'] ?>
-                        </h5>
+                            <tbody>
+                                <?php
+                                while ($row = $result->fetch_assoc()) {
+                                    ?>
+                                    <tr>
+                                        <td><?= $row['date'] ?></td>
+                                        <td><?= $row['kode'] ?></td>
+                                        <td><?= $row['jumlah_beli'] ?></td>
+                                        <td><?= $row['harga_beli'] ?></td>
+                                        <td><?= $row['harga_beli'] * $row['jumlah_beli'] ?></td>
+                                        <td><?= $row['jumlah_jual'] ?></td>
+                                        <td><?= $row['harga_jual'] ?></td>
+                                        <td><?= $row['harga_jual'] * $row['jumlah_jual'] ?></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
                     </div>
-                    <div class="card-body p-4">
-                        <h5 class="card-title"><?= $row['kode_produk'] ?> - <?= $row['nama_produk'] ?></h5>
-                        <p class="card-text">
-                            <b>Stok</b> : <?= $row['jumlah'] ?>
-                            <br>
-                            <b>Harga</b> : <?= $row['harga_beli'] ?>
-                            <br>
-                            <b>Tanggal Masuk</b> : <?= $row['tanggal_masuk'] ?>
-                            <br>
-                            <b>Tanggal Kadaluarsa</b> : <?= $row['tanggal_kadaluarsa'] ?>
-                        </p>
-                    </div>
-
                 </div>
             </div>
-        <?php } ?>
+        </div>
     </div>
-</div>

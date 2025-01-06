68c05a8dae90a9655ad6aa45323b9fed041a7fbe
Quarnyx <anugrahsandysulmanpratama@gmail.com>
2025-01-06 17:35:16 +0700

    rev 4.2

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "apotik";
+$dbname = "apotek";
 
 // Buat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
